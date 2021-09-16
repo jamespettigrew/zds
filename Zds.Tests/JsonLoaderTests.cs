7365a3a95c1b2bcc6a1f521fa74dc77282f0c8a2
James Pettigrew <james@innovum.com.au>
2021-09-16 23:53:01 +1000

    Add JSON loading functionality & tests.

---
+++ Zds.Tests/JsonLoaderTests.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Zds.Tests
+{
+    [TestClass]
+    public class JsonLoaderTests
+    {
+        private static string TestJson = @"[
+            {
+                ""_id"": ""436bf9b0-1147-4c0a-8439-6f79833bff5b"",
+                ""type"": ""incident"",
+                ""tags"": [
+                    ""Ohio"",
+                    ""Pennsylvania""
+                ]
+            },
+            {
+                ""_id"": ""1a227508-9f39-427c-8f57-1b72f3fab87c"",
+                ""type"": ""incident"",
+                ""tags"": [
+                    ""Puerto Rico"",
+                    ""Idaho""
+                ]
+            }
+        ]";
+
+        private static string InvalidTestJson = @"[zxcvzxv}{]";
+        
+        [TestMethod]
+        public void EnumeratedObjectsHaveCorrectPositions()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(TestJson));
+            List<Position> positions = JsonLoader
+                .EnumerateObjects(stream)
+                .Select(o => o.Position)
+                .ToList();
+
+            CollectionAssert.AreEquivalent(new Position[] {new(2, 13), new(10, 13)}, positions);
+        }
+        
+        
+        [TestMethod]
+        public void EnumeratedObjectsHaveCorrectPathValues()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(TestJson));
+            List<ObjectRecord> objectRecords = JsonLoader.EnumerateObjects(stream).ToList();
+            
+            List<PathValue> expected = new()
+            {
+                new("_id", "436bf9b0-1147-4c0a-8439-6f79833bff5b"),
+                new("type", "incident")
+            };
+            CollectionAssert.AreEquivalent(expected, objectRecords[0].PathValues.ToList());
+            
+            expected = new()
+            {
+                new("_id", "1a227508-9f39-427c-8f57-1b72f3fab87c"),
+                new("type", "incident")
+            };
+            CollectionAssert.AreEquivalent(expected, objectRecords[1].PathValues.ToList());
+        }
+        
+        [TestMethod]
+        public void EnumeratedObjectsFromInvalidJsonThrowsError()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(InvalidTestJson));
+            Assert.ThrowsException<JsonLoaderException>(() => JsonLoader.EnumerateObjects(stream).ToList());
+        }
+        
+        [TestMethod]
+        public void GetObjectAtValidPositionReturnsCorrectObject()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(TestJson));
+            JObject expected = JsonConvert.DeserializeObject<List<JObject>>(TestJson)![1];
+            JObject actual = JsonLoader.GetObjectStartingAtPosition(stream, new Position(10, 13));
+
+            Assert.IsTrue(JObject.DeepEquals(expected, actual));
+        }
+        
+        [TestMethod]
+        public void GetObjectAtInvalidPositionThrowsError()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(TestJson));
+            Assert.ThrowsException<JsonLoaderException>(() =>
+                JsonLoader.GetObjectStartingAtPosition(stream, new Position(1, 1)));
+        }
+        
+        [TestMethod]
+        public void GetObjectAtValidPositionFromInvalidJsonThrowsError()
+        {
+            MemoryStream stream = new(Encoding.UTF8.GetBytes(InvalidTestJson));
+            Assert.ThrowsException<JsonLoaderException>(() =>
+                JsonLoader.GetObjectStartingAtPosition(stream, new Position(3, 5)));
+        }
+    }
+}
---
+++ Zds/JsonLoader.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Zds
+{
+    public record Position(int Line, int LinePosition);
+    public record ObjectRecord(Position Position, List<PathValue> PathValues);
+    public record PathValue(string Path, string Value);
+
+    public class JsonLoaderException : Exception
+    {
+        public JsonLoaderException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+    
+    public static class JsonLoader
+    {
+        public static IEnumerable<ObjectRecord> EnumerateObjects(Stream stream)
+        {
+            using StreamReader sr = new(stream);
+            using JsonTextReader reader = new(sr);
+
+            while (true)
+            {
+                ObjectRecord? record = null;
+                try
+                {
+                    if (!reader.Read()) break;
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        Position p = new(reader.LineNumber, reader.LinePosition);
+                        record = new ObjectRecord(p, Flatten(JObject.Load(reader)));
+                    }
+                }
+                catch (JsonException e)
+                {
+                    throw new JsonLoaderException("Error encountered enumerating JSON objects.", e);
+                }
+
+                // If this looks clumsy it's because C# doesn't permit yielding within a try-catch.
+                if (record != null) yield return record;
+            }
+        }
+
+        private static List<PathValue> Flatten(JObject obj)
+        {
+            Stack<JToken> stack = new();
+            stack.Push(obj);
+
+            List<PathValue> pathValues = new ();
+            while (stack.Count > 0)
+            {
+                JToken token = stack.Pop();
+                switch (token.Type)
+                {
+                    // Treat arrays as out of scope for now
+                    case JTokenType.Array:
+                        continue;
+                    case JTokenType.Object:
+                    case JTokenType.Property:
+                        foreach (JToken child in token.Children())
+                        {
+                            stack.Push(child);
+                        }
+                        break;
+                    default:
+                        pathValues.Add(new (token.Path, token.ToString()));
+                        break;
+                }
+            }
+
+            return pathValues;
+        }
+
+        public static JObject GetObjectStartingAtPosition(Stream stream, Position position)
+        {
+            using StreamReader sr = new(stream);
+            try
+            {
+                // Consume stream, by line and character, until we reach first token of the object we wish to load.
+                for (int i = 1; i < position.Line; i++) { sr.ReadLine(); }
+                for (int i = 1; i < position.LinePosition; i++) { sr.Read(); }
+
+                using JsonTextReader reader = new(sr);
+                return JObject.Load(reader);
+            }
+            catch (Exception e) when (e is JsonReaderException || e is IOException || e is OutOfMemoryException)
+            {
+                throw new JsonLoaderException("Error encountered loading JSON object at position.", e);
+            }
+        }
+    }
+}
