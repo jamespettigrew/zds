bdab51928ae33171eb5f02aa70a1f59c1c42bf20
James Pettigrew <james@innovum.com.au>
2021-09-18 22:05:59 +1000

    Add pagination to query results view.

--- Zds/Cli/DisplayQueryResultsCommand.cs
+++ Zds/Cli/DisplayQueryResultsCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json.Linq;
 using Spectre.Console;
+using Spectre.Console.Rendering;
 using Zds.Core;
 
 namespace Zds.Cli
@@ -23,17 +24,116 @@ namespace Zds.Cli
             r.Alignment = Justify.Left;
             AnsiConsole.Render(r);
 
-            for (int i = 0; i < matches.Count; i++)
+            Page page = new (matches.Count, 3);
+            AnsiConsole.Render(GenerateRenderableForPage(selectedSource, matches, page));
+            
+            bool displayingResults = true;
+            while (displayingResults)
             {
-                FileStream stream = File.Open(selectedSource, FileMode.Open);
-                JObject jobj = JsonLoader.GetObjectStartingAtPosition(stream, matches[i]);
-                string escaped = Markup.Escape(jobj.ToString());
-                var panel = new Panel(escaped);
-                panel.Header = new PanelHeader($"[darkorange]Match[/] {i + 1}/{matches.Count}");
-                AnsiConsole.Render(panel);
+                switch (Console.ReadKey())
+                {
+                    case {Key: ConsoleKey.LeftArrow}:
+                        var prevPage = page.Prev();
+                        if (prevPage != null)
+                        {
+                            page = prevPage;
+                            AnsiConsole.Render(GenerateRenderableForPage(selectedSource, matches, page));
+                        }
+                        break;
+                    case {Key: ConsoleKey.RightArrow}:
+                        var nextPage = page.Next();
+                        if (nextPage != null)
+                        {
+                            page = nextPage;
+                            AnsiConsole.Render(GenerateRenderableForPage(selectedSource, matches, page));
+                        }
+                        break;
+                    default:
+                        displayingResults = false;
+                        break;
+                }
             }
 
             new SelectSourceCommand(_repository).Execute();
+        }
+
+        private record Page
+        {
+            private int _total;
+            private int _size;
+            
+            public Page(int total, int size)
+            {
+                _total = total;
+                _size = size;
+                Start = 0;
+                End = _size;
+            }
+
+            public int Start { get; init; }
+            public int End { get; init; }
+            
+            public Page? Prev()
+            {
+                int prevStart = Math.Max(0, Start - _size);
+                int prevEnd = Math.Min(_total, Start);
+                if (prevEnd <= prevStart) return null;
+                return this with {
+                    Start = prevStart,
+                    End = prevEnd
+                };
+            }
+
+            public Page? Next()
+            {
+                int nextStart = End;
+                int nextEnd = Math.Min(_total, End + _size);
+                if (nextStart >= nextEnd) return null;
+                return this with {
+                    Start = nextStart,
+                    End = nextEnd
+                };
+            }
+        }
+        
+        private IRenderable GenerateRenderableForPage(string source, List<Position> matches, Page page)
+        {
+            var table = new Table { Border = TableBorder.Rounded };
+            table.AddColumn("Page");
+
+            for (int i = page.Start; i < page.End; i++)
+            {
+                var tree = new Tree(new Text($"{i + 1}", Style.Parse("darkorange")))
+                {
+                   Style = Style.Parse("darkorange")
+                };
+                
+                FileStream stream = File.Open(source, FileMode.Open);
+                JObject jobj = JsonLoader.GetObjectStartingAtPosition(stream, matches[i]);
+                string escaped = Markup.Escape(jobj.ToString());
+                var panel = new Panel(escaped)
+                {
+                    Border = BoxBorder.Rounded,
+                    BorderStyle = Style.Parse("darkorange")
+                };
+                tree.AddNode(panel);
+                table.AddRow(tree);
+            }
+
+            var footer = new Table
+            {
+                Border = TableBorder.None,
+                BorderStyle = Style.Parse("darkorange")
+            };
+            footer.AddColumn("");
+            footer.HideHeaders();
+            footer.AddRow(new Text($"Displaying results {page.Start + 1}-{page.End} / {matches.Count}"));
+            if (page.Prev() != null) footer.AddRow(new Text("Prev (←)"));
+            if (page.Next() != null) footer.AddRow(new Text("Next (→)"));
+            footer.AddRow(new Text("New Search (Any other key)"));
+            table.AddRow(footer);
+            
+            return table;
         }
     }
 }
