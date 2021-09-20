98d00c6a839f08136d8da21e3e0b58fc0624edc8
James Pettigrew <james@innovum.com.au>
2021-09-20 16:00:28 +1000

    Add RelationsRepository tests.

---
+++ Zds.Tests/RelationsRepositoryTests.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zds.Core;
+using Zds.Core.Queries;
+using Zds.Core.Relations;
+
+namespace Zds.Tests
+{
+    [TestClass]
+    public class RelationsRepositoryTests
+    {
+        [TestMethod]
+        public void RelatedQueriesCorrect()
+        {
+            RelationsRepository repository = new();
+            CollectionAssert.AreEquivalent(
+                new List<ObjectGraphQuery>(),
+                repository.ComputeRelatedQueries("test", new PathValue[] { })
+            );
+
+            List<Relation> relations = new List<Relation>
+            {
+                new (new Key("test", "a"), new Key("test2", "b"))
+            };
+            repository.AddRelations(relations);
+            var pathValues = new PathValue[]
+            {
+                new ("a", "1"),
+                new ("b", "2")
+            };
+            
+            CollectionAssert.AreEquivalent(
+                new List<ObjectGraphQuery>(),
+                repository.ComputeRelatedQueries("other", pathValues)
+            );
+            CollectionAssert.AreEquivalent(
+                new List<ObjectGraphQuery> { new ("test2", "b", "1") },
+                repository.ComputeRelatedQueries("test", pathValues)
+            );
+        }
+    }
+}
